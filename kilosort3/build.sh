271eb5e459cf45a7a45d99e41208ac79d59e8aaf
Jeremy Magland <jeremy.magland@gmail.com>
2021-04-14 10:50:33 -0400

    rename image to kilosort3-base

--- kilosort3/build.sh
+++ kilosort3/build.sh
@@ -2,4 +2,4 @@
 
 # usually the tag would be the version of the sorter
 # but in this case since 3 is already in the name, maybe the tag should be 0.1.x
-docker build -t spikeinterface/kilosort3:0.1.0 .
+docker build -t spikeinterface/kilosort3-base:0.1.0 .
--- kilosort3/push.sh
+++ kilosort3/push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker push spikeinterface/kilosort3:0.1.0
+docker push spikeinterface/kilosort3-base:0.1.0
