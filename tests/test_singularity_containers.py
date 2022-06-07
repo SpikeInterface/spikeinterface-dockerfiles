5dfd7ecbf70647774148ecbe9a50e479f13e5380
bendichter <ben.dichter@gmail.com>
2022-06-07 17:39:34 -0400

    use generate_recording

--- tests/test_singularity_containers.py
+++ tests/test_singularity_containers.py
@@ -1,7 +1,15 @@
-from spikeinterface.extractors import toy_example
+#from spikeinterface.extractors import toy_example
 import spikeinterface.sorters as ss
+from spikeinterface.core.testing_tools import generate_recording
 
-recording = toy_example(num_segments=1, num_channels=32, duration=120)[0]
+recording = generate_recording(
+    num_channels=32,
+    sampling_frequency=30000.,  # in Hz
+    durations=[120],  #  in s for 2 segments
+    set_probe=True,
+    ndim=2,
+)
+#recording = toy_example(num_segments=1, num_channels=32, duration=120)[0]
 recording2 = recording.save(folder="test_recording")
 # recording2 = si.load_extractor("test_recording11")
 
