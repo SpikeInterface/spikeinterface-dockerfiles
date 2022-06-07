e456c55ea9530dad25ec71eec943b16d19ed508c
bendichter <ben.dichter@gmail.com>
2022-06-07 16:56:09 -0400

    add ci tests

---
+++ tests/test_singularity_containers.py
@@ -0,0 +1,16 @@
+import spikeinterface.full as si
+import spikeinterface.sorters as ss
+
+recording = si.toy_example(num_segments=1, num_channels=32, duration=120)[0]
+recording2 = recording.save("test_recording")
+# recording2 = si.load_extractor("test_recording11")
+
+
+def test_spyking_circus():
+
+    ss.run_spykingcircus(
+        recording2,
+        output_folder="spyking_circus",
+        singularity_image="spikeinterface/spyking-circus-base:1.1.0",
+        verbose=True
+    )
