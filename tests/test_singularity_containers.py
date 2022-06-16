f241e18c80860686a5dd0386d4d21925e155041a
bendichter <ben.dichter@gmail.com>
2022-06-15 23:03:16 -0400

    refactor tests

--- tests/test_singularity_containers.py
+++ tests/test_singularity_containers.py
@@ -13,102 +13,58 @@ recording = generate_recording(
 
 recording2 = recording.save(folder="test_recording")
 
-# python-based
+kwargs = dict(recordin=recording2, verbose=True, singularity_image=True)
+
+
 @pytest.mark.xfail(reason="FAILING: investigate why")
 def test_spyking_circus():
-
-    ss.run_spykingcircus(
-        recording2, output_folder="spyking_circus", singularity_image=True, verbose=True
-    )
-
-
-def test_tridescluos():
-
-    ss.run_tridesclous(
-        recording2, output_folder="tridesclous", singularity_image=True, verbose=True
-    )
-
-
-def test_klusta():
-
-    ss.run_klusta(
-        recording2, output_folder="klusta", singularity_image=True, verbose=True
-    )
+    ss.run_spykingcircus(output_folder="spyking_circus", **kwargs)
 
 
 @pytest.mark.xfail(reason="FAILING: investigate why")
 def test_mountainsort4():
-
-    ss.run_mountainsort4(
-        recording2, output_folder="mountainsort4", singularity_image=True, verbose=True
-    )
+    ss.run_mountainsort4(output_folder="mountainsort4", **kwargs)
 
 
-# matlab-based
+def test_tridesclous():
+    ss.run_tridesclous(output_folder="tridesclous", **kwargs)
+
+
+def test_klusta():
+    ss.run_klusta(output_folder="klusta", **kwargs)
+
+
 def test_ironclust():
-
-    ss.run_ironclust(
-        recording2,
-        output_folder="ironclust",
-        singularity_image=True,
-        fGpu=False,
-        verbose=True,
-    )
+    ss.run_ironclust(output_folder="ironclust", fGpu=False, **kwargs)
 
 
 def test_waveclus():
-
-    ss.run_waveclus(
-        recording2, output_folder="waveclus", singularity_image=True, verbose=True
-    )
+    ss.run_waveclus(output_folder="waveclus", **kwargs)
 
 
 def test_hdsort():
-
-    ss.run_hdsort(
-        recording2, output_folder="hdsort", singularity_image=True, verbose=True
-    )
+    ss.run_hdsort(output_folder="hdsort", **kwargs)
 
 
 def test_kilosort1():
-
-    ss.run_kilosort(
-        recording2,
-        output_folder="kilosort",
-        singularity_image=True,
-        useGPU=False,
-        verbose=True,
-    )
+    ss.run_kilosort(output_folder="kilosort", useGPU=False, **kwargs)
 
 
-# gpu-required
 @pytest.mark.skip(reason="GPU required")
 def test_kilosort2():
-
-    ss.run_kilosort2(
-        recording2, output_folder="kilosort2", singularity_image=True, verbose=True
-    )
+    ss.run_kilosort2(output_folder="kilosort2", **kwargs)
 
 
 @pytest.mark.skip(reason="GPU required")
 def test_kilosort2_5():
-
-    ss.run_kilosort2_5(
-        recording2, output_folder="kilosort2_5", singularity_image=True, verbose=True
-    )
+    ss.run_kilosort2_5(output_folder="kilosort2_5", **kwargs)
 
 
 @pytest.mark.skip(reason="GPU required")
 def test_kilosort3():
-
-    ss.run_kilosort3(
-        recording2, output_folder="kilosort3", singularity_image=True, verbose=True
-    )
+    ss.run_kilosort3(output_folder="kilosort3", **kwargs)
 
 
 @pytest.mark.skip(reason="GPU required")
 def test_pykilosort():
-
-    ss.run_pykilosort(
-        recording2, output_folder="pykilosort", singularity_image=True, verbose=True
-    )
+    ss.run_pykilosort(output_folder="pykilosort", **kwargs)
