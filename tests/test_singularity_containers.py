b17afae6322663f9258cdba9d3f0de2c6d9e3661
Chuang Yu Min <chg.yumin@gmail.com>
2022-06-16 09:39:49 -0300

    Added work_dir and recording fixtures

--- tests/test_singularity_containers.py
+++ tests/test_singularity_containers.py
@@ -1,42 +1,59 @@
+import os
+import shutil
+
+import pytest
+
 import spikeinterface.sorters as ss
 from spikeinterface.core.testing_tools import generate_recording
 
-recording = generate_recording(
-    num_channels=32,
-    sampling_frequency=30000.,  # in Hz
-    durations=[120],
-    set_probe=True,
-    ndim=2,
-)
-#recording = toy_example(num_segments=1, num_channels=32, duration=120)[0]
-recording2 = recording.save(folder="test_recording")
-# recording2 = si.load_extractor("test_recording11")
+
+@pytest.fixture
+def work_dir(request, tmpdir_factory):
+    tmpdir = tmpdir_factory.mktemp('work_dir')
+    os.chdir(tmpdir)
+    yield
+    os.chdir(request.config.invocation_dir)
+    shutil.rmtree(str(tmpdir))
 
 
-def test_spyking_circus():
+@pytest.fixture
+def recording(work_dir):
+
+    test_recording = generate_recording(
+        num_channels=32,
+        sampling_frequency=30000.0,  # in Hz
+        durations=[120],
+        set_probe=True,
+        ndim=2,
+    )
+    test_recording = test_recording.save(folder="test_recording")
+    return test_recording
+
+
+def test_spyking_circus(recording, work_dir):
 
     ss.run_spykingcircus(
-        recording2,
+        recording,
         output_folder="spyking_circus",
         singularity_image="spikeinterface/spyking-circus-base:1.1.0",
         verbose=True
     )
 
 
-def test_tridecluos():
+def test_tridecluos(recording):
 
     ss.run_tridesclous(
-        recording2,
+        recording,
         output_folder="tridesclous",
         singularity_image="spikeinterface/tridesclous-base:1.6.5",
         verbose=True
     )
 
 
-def test_ironclust():
+def test_ironclust(recording):
 
     ss.run_ironclust(
-        recording2,
+        recording,
         output_folder="ironclust",
         singularity_image="spikeinterface/ironclust-compiled-base:5.9.8",
         verbose=True
