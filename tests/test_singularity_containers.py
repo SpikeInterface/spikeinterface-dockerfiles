8b6bd8793a56b039920e339575b0afd11012286c
Chuang Yu Min <chg.yumin@gmail.com>
2022-06-17 16:42:08 -0300

    reduce duration, install tridesclous, print sorting

--- tests/test_singularity_containers.py
+++ tests/test_singularity_containers.py
@@ -19,7 +19,7 @@ def work_dir(request, tmpdir_factory):
 @pytest.fixture
 def run_kwargs(work_dir):
     test_recording, _ = se.toy_example(
-        duration=120,
+        duration=30,
         seed=0,
         num_channels=32,
         num_segments=1
@@ -29,53 +29,65 @@ def run_kwargs(work_dir):
 
 
 def test_spyking_circus(run_kwargs):
-    ss.run_spykingcircus(output_folder="spyking_circus", **run_kwargs)
+    sorting = ss.run_spykingcircus(output_folder="spyking_circus", **run_kwargs)
+    print(sorting)
 
 
 @pytest.mark.xfail(reason="FAILING: investigate why")
 def test_mountainsort4(run_kwargs):
-    ss.run_mountainsort4(output_folder="mountainsort4", **run_kwargs)
+    sorting = ss.run_mountainsort4(output_folder="mountainsort4", **run_kwargs)
+    print(sorting)
 
 
 def test_tridesclous(run_kwargs):
-    ss.run_tridesclous(output_folder="tridesclous", **run_kwargs)
+    sorting = ss.run_tridesclous(output_folder="tridesclous", **run_kwargs)
+    print(sorting)
 
 
 def test_klusta(run_kwargs):
-    ss.run_klusta(output_folder="klusta", **run_kwargs)
+    sorting = ss.run_klusta(output_folder="klusta", **run_kwargs)
+    print(sorting)
 
 
 def test_ironclust(run_kwargs):
-    ss.run_ironclust(output_folder="ironclust", fGpu=False, **run_kwargs)
+    sorting = ss.run_ironclust(output_folder="ironclust", fGpu=False, **run_kwargs)
+    print(sorting)
 
 
 def test_waveclus(run_kwargs):
-    ss.run_waveclus(output_folder="waveclus", **run_kwargs)
+    sorting = ss.run_waveclus(output_folder="waveclus", **run_kwargs)
+    print(sorting)
 
 
 def test_hdsort(run_kwargs):
-    ss.run_hdsort(output_folder="hdsort", **run_kwargs)
+    sorting = ss.run_hdsort(output_folder="hdsort", **run_kwargs)
+    print(sorting)
 
 
 def test_kilosort1(run_kwargs):
-    ss.run_kilosort(output_folder="kilosort", useGPU=False, **run_kwargs)
+    sorting = ss.run_kilosort(output_folder="kilosort", useGPU=False, **run_kwargs)
+    print(sorting)
 
 
 @pytest.mark.skip(reason="GPU required")
 def test_kilosort2(run_kwargs):
-    ss.run_kilosort2(output_folder="kilosort2", **run_kwargs)
+    sorting = ss.run_kilosort2(output_folder="kilosort2", **run_kwargs)
+    print(sorting)
 
 
 @pytest.mark.skip(reason="GPU required")
 def test_kilosort2_5(run_kwargs):
-    ss.run_kilosort2_5(output_folder="kilosort2_5", **run_kwargs)
+    sorting = ss.run_kilosort2_5(output_folder="kilosort2_5", **run_kwargs)
+    print(sorting)
 
 
 @pytest.mark.skip(reason="GPU required")
 def test_kilosort3(run_kwargs):
-    ss.run_kilosort3(output_folder="kilosort3", **run_kwargs)
+    sorting = ss.run_kilosort3(output_folder="kilosort3", **run_kwargs)
+    print(sorting)
 
 
 @pytest.mark.skip(reason="GPU required")
 def test_pykilosort(run_kwargs):
-    ss.run_pykilosort(output_folder="pykilosort", **run_kwargs)
+    sorting = ss.run_pykilosort(output_folder="pykilosort", **run_kwargs)
+    print(sorting)
