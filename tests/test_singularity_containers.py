92c2cbee655158a8dfa61d21b65862d33540386f
Chuang Yu Min <chg.yumin@gmail.com>
2022-06-20 15:08:41 -0300

    skip ks1

--- tests/test_singularity_containers.py
+++ tests/test_singularity_containers.py
@@ -64,6 +64,7 @@ def test_hdsort(run_kwargs):
     print(sorting)
 
 
+@pytest.mark.skip(reason='Image for KiloSort 1 not implemented yet')
 def test_kilosort1(run_kwargs):
     sorting = ss.run_kilosort(output_folder="kilosort", useGPU=False, **run_kwargs)
     print(sorting)
