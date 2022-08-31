689e7c026e10d9daf30c0766fe1fb8c7258c74af
Chuang Yu Min <chg.yumin@gmail.com>
2022-08-31 09:12:14 -0300

    Add compile script, remove matlab file - waveclus

---
+++ waveclus-compiled/compile.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+if [ $# == 0 ]; then
+    echo "Usage: $0 param1 param2"
+    echo "* param1: waveclus path"
+    echo "* param2: spikeinterface path"
+    exit
+fi
+
+if [ $# -ne 2 ]; then
+    echo "spikeinterface and waveclus path must be given"
+    exit 1
+fi
+
+WC_PATH=$1
+SI_PATH=$2
+WORK_DIR=$(pwd)
+SOURCE_DIR=$( dirname -- "$0"; )
+TMP_DIR=$SOURCE_DIR/tmp
+
+echo "waveclus path: ${WC_PATH}"
+echo "spike-interface path: ${SI_PATH}"
+
+echo "Creating tmp folder: $TMP_DIR"
+cd $WORK_DIR
+mkdir -p $TMP_DIR
+
+echo "Compiling waveclus_master..."
+cd $TMP_DIR
+matlab -batch "mcc -m ${SI_PATH}/spikeinterface/sorters/waveclus/waveclus_master.m -a ${SI_PATH}/spikeinterface/sorters/utils -a ${WC_PATH}"
+
+echo "Creating base docker image..."
+matlab -batch "compiler.package.docker('waveclus_master', 'requiredMCRProducts.txt', 'ImageName', 'waveclus-matlab-base')"
+
+cd $WORK_DIR
+rm -r $TMP_DIR
