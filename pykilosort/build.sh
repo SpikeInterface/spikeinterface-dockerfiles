b1bb68bde362ece18f1e5c7a3e488b57c950bc78
Vincent <prevosto@mit.edu>
2023-04-21 15:39:31 -0400

    updates pykilosort

--- pykilosort/build.sh
+++ pykilosort/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build -t spikeinterface/pykilosort-base:latest -t spikeinterface/pykilosort-base:ibl-1.3 .
+# docker build -t spikeinterface/pykilosort-base:latest -t spikeinterface/pykilosort-base:ibl-1.3 .
+docker build -t wanglabneuro/pykilosort-base:latest -t wanglabneuro/pykilosort-base:ibl-1.3 .
--- pykilosort/push.sh
+++ pykilosort/push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker push --all-tags spikeinterface/pykilosort-base
+# docker push --all-tags spikeinterface/pykilosort-base
+docker push --all-tags wanglabneuro/pykilosort-base
