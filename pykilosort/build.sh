92b29eb957604e91760fecb6a4078a225542eb15
Alessio Buccino <alejoe9187@gmail.com>
2023-08-01 14:52:20 +0200

    Remove cupy prints from pyks

--- pykilosort/build.sh
+++ pykilosort/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t spikeinterface/pykilosort-base:latest -t spikeinterface/pykilosort-base:1.4.3 .
+docker build -t spikeinterface/pykilosort-base:latest -t spikeinterface/pykilosort-base:1.4.3.1 .
