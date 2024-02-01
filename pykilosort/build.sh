6a624345b5dc0ff08501ff22e1937da5a5f8b26f
Vincent <prevosto@mit.edu>
2024-02-01 12:56:45 -0500

    for PR

--- pykilosort/build.sh
+++ pykilosort/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# docker build -t spikeinterface/pykilosort-base:latest -t spikeinterface/pykilosort-base:ibl-1.3 .
-# docker build -t wanglabneuro/pykilosort-base:latest -t wanglabneuro/pykilosort-base:ibl-1.3 .
-# docker build -t wanglabneuro/pykilosort-base:latest -t wanglabneuro/pykilosort-base:ibl-1.4.7_cuda-11.3.1 .
-docker build -t wanglabneuro/pykilosort-base:latest -t wanglabneuro/pykilosort-base:ibl-1.4.7_cuda-11.5.2 .
+docker build -t spikeinterface/pykilosort-base:latest -t spikeinterface/pykilosort-base:ibl-1.4.7_cuda-11.5.2 .
 
 # version info
 # 01/21/2024 - IBL 1.4.7 CUDA 11.3.1 Ubuntu 20.04
