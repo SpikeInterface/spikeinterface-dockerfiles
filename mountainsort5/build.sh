4285e869c4a4e6f11b76cba765bf955b77d379e4
luiz <luiz_tauffer@hotmail.com>
2024-04-03 19:00:28 +0200

    update mountainsort5 image

--- mountainsort5/build.sh
+++ mountainsort5/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t spikeinterface/mountainsort5-base:latest -t spikeinterface/mountainsort5-base:0.5.3 .
+docker build -t spikeinterface/mountainsort5-base:latest -t spikeinterface/mountainsort5-base:0.5.6 .
