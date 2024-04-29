f01a92d95b26ebd0b17381264c8032633453fd3f
Alessio Buccino <alejoe9187@gmail.com>
2024-04-29 13:29:37 +0200

    Change image versions to reflect KS tags

--- kilosort-compiled/kilosort2-compiled/build.sh
+++ kilosort-compiled/kilosort2-compiled/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t spikeinterface/kilosort2-compiled-base:latest -t spikeinterface/kilosort2-compiled-base:0.2.0 .
+docker build -t spikeinterface/kilosort2-compiled-base:latest -t spikeinterface/kilosort2-compiled-base:2.0.2 .
 
--- kilosort-compiled/kilosort3-compiled/build.sh
+++ kilosort-compiled/kilosort3-compiled/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t spikeinterface/kilosort3-compiled-base:latest -t spikeinterface/kilosort3-compiled-base:0.3.0 .
+docker build -t spikeinterface/kilosort3-compiled-base:latest -t spikeinterface/kilosort3-compiled-base:3.0.2 .
