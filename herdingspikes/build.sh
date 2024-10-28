a856256f8f69c88c9d28cbd0e585b49fc0464564
Alessio Buccino <alejoe9187@gmail.com>
2024-10-28 18:35:26 +0100

    Update herdingspikes version

--- herdingspikes/build.sh
+++ herdingspikes/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t spikeinterface/herdingspikes-base:latest -t spikeinterface/herdingspikes-base:0.4.3 .
+docker build -t spikeinterface/herdingspikes-base:latest -t spikeinterface/herdingspikes-base:0.4.5 .
