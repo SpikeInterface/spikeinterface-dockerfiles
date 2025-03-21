06073d33cb132e8a5b01b1943a1cd2d405a1b510
Alessio Buccino <alejoe9187@gmail.com>
2025-03-21 14:37:46 +0100

    Better logic to handle KS4 version and update to 4.0.30

--- kilosort4/build.sh
+++ kilosort4/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build -t spikeinterface/kilosort4-base:latest -t spikeinterface/kilosort4-base:4.0.18_cuda-12.0.0 .
+# Extract the KILOSORT4_VERSION from the Dockerfile
+KILOSORT4_VERSION=$(grep -E '^ENV KILOSORT4_VERSION=' "Dockerfile" | awk -F= '{print $2}' | tr -d '[:space:]')
+echo "Building kilosort4-base:$KILOSORT4_VERSION"
+
+docker build -t spikeinterface/kilosort4-base:latest -t spikeinterface/kilosort4-base:${KILOSORT4_VERSION}_cuda-12.0.0 .
