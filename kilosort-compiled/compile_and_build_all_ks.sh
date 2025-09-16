c03839023bd4cb8093e29fdfc759c4c7e077415d
Alessio Buccino <alejoe9187@gmail.com>
2025-09-16 17:08:29 +0200

    Update Kilosort-compiled images

--- kilosort-compiled/compile_and_build_all_ks.sh
+++ kilosort-compiled/compile_and_build_all_ks.sh
@@ -23,6 +23,7 @@ KILOSORT2_PATH=${3%/}
 KILOSORT25_PATH=${4%/}
 KILOSORT3_PATH=${5%/}
 
+# export MW_NVCC_PATH=/usr/local/cuda-11.2/bin/
 export MW_NVCC_PATH=/usr/bin/
 export MW_ALLOW_ANY_CUDA=1
 
--- kilosort-compiled/kilosort-compiled/build.sh
+++ kilosort-compiled/kilosort-compiled/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t spikeinterface/kilosort-compiled-base:latest -t spikeinterface/kilosort-compiled-base:0.2.0 .
+docker build -t spikeinterface/kilosort-compiled-base:latest -t spikeinterface/kilosort-compiled-base:1.0.0_cuda-11.2.2 .
 
